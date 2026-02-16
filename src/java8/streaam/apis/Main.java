e417c570e40c889dd7e5cfde92c97294bdc16361
api-code <madavi.apekshas@gmail.com>
2026-02-17 00:18:21 +0530

    added stream api questions

--- src/java8/streaam/apis/Main.java
+++ src/java8/streaam/apis/Main.java
@@ -1,5 +1,8 @@
 package java8.streaam.apis;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class Main {
     public static void main(String[] args) {
         String s = "I am learning stream api in java";
@@ -7,6 +10,17 @@ public class Main {
 //        System.out.println(count);
 
         String sRemoveDup = "dabcadefg";
-        StreamsQuestions.removeDuplicate(sRemoveDup);
+//        StreamsQuestions.removeDuplicate(sRemoveDup);
+
+        String secondHighLen = "I am learning streams api in java";
+        String secondHighestLength = StreamsQuestions.secondHighestLen(secondHighLen);
+        System.out.println(secondHighestLength);
+
+        String secondHighLenCount = "I am learning streams api in java";
+        int secondHighestLengthCount = StreamsQuestions.secondHighestLenCount(secondHighLenCount);
+//        System.out.println(secondHighestLengthCount);
+
+        String wordOccurence = "I am api learning streams java api in java";
+        StreamsQuestions.wordOccurrence(wordOccurence);
     }
 }
--- src/java8/streaam/apis/StreamsQuestions.java
+++ src/java8/streaam/apis/StreamsQuestions.java
@@ -1,7 +1,12 @@
 package java8.streaam.apis;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class StreamsQuestions {
     public static String highestLength(String s){
@@ -12,6 +17,31 @@ public class StreamsQuestions {
     }
 
     public static void removeDuplicate(String s){
-        s.chars().distinct().mapToObj(x->(char)x).forEach(System.out::print);
+//        s.chars().distinct().mapToObj(x->(char)x).forEach(System.out::print);
+        Arrays.stream(s.split("")).distinct().forEach(System.out::print);
+    }
+
+    public static String secondHighestLen(String s){
+        String answer = Arrays.stream(s.split(" ")).
+                sorted(Comparator.comparing(String::length).reversed())
+                .skip(0).findFirst().get();
+
+        return answer;
+    }
+
+    public static int secondHighestLenCount(String s) {
+        int count = Arrays.stream(s.split(" ")).
+                sorted(Comparator.comparing(String::length).reversed())
+                .skip(1).findFirst().get().length();
+
+        return count;
+    }
+
+    public static void wordOccurrence(String s){
+
+        Map<String,Long> map = Arrays.stream(s.split(" "))
+                .collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+
+        System.out.println(map);
     }
 }
